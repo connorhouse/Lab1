1899ac338ea99191417ac66877c68219b4bc4460
Connor House <connorhouse19@gmail.com>
2019-09-08 18:21:30 -0400

    a

---
+++ drawCircle.py
@@ -0,0 +1,18 @@
+import turtle
+
+window = turtle.Screen()
+
+turtle.bgcolor("black")
+turtle.color("white" , "black")
+
+turtle.begin_fill()
+turtle.circle(200)
+turtle.end_fill()
+
+turtle.begin_fill()
+turtle.circle(100)
+turtle.end_fill()
+
+turtle.begin_fill()
+turtle.circle(50)
+turtle.end_fill()
