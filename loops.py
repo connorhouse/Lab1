4c23afe90261fccf2e8aa7f3db6bdbc5333566fb
connorhouse <35511674+connorhouse@users.noreply.github.com>
2019-09-09 11:57:06 -0400

    Update loops.py

--- loops.py
+++ loops.py
@@ -1,8 +1,10 @@
-for x in range(1,11,2):
+for x in range(1,11,1):
         print(x)
+print("------")
 
-for y in range(20,14,1):
+for y in range(20,14,-1):
         print(y)
+print("------")
 
 for z in range(0,101,2):
         print(z)
